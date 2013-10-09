f2646ff02f446abee2e9752398e79ae40687d6c4
Rath Kaikala <rathk@fullsail.edu>
2013-10-09 15:33:33 -0700

    updating and push of Style Tile and index.php

---
+++ procipe/api-proof-concept.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head><title>Proof of API Concept</title></head>
+<?php
+
+?>
+<body>
+	<h1>Procipe API Proof of Concept</h1>
+	<h4>MDD1310 - Rath Kaikala</h4>
+	<form action="" method="post">
+		<label for="search">Enter Recipe Search Terms</label>
+			<input type="text" name="search" id="search"/><br/>
+		<input type="submit" name="submit"/>
+	</form>
+</body>
+
+</html>
---
+++ procipe/index.php
@@ -0,0 +1,3 @@
+<?php
+/* Main content goes on this page. */
+?>
